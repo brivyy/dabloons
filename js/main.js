72afc64c90fa6a92db5a794f7e18701b942768dc
brivyy <vieirabruno28112005@gmail.com>
2022-12-16 22:11:26 -0300

    abrir menus

--- js/main.js
+++ js/main.js
@@ -1,5 +1,29 @@
-$("#outrosmenus").click(() => {
+function abrir(divs) {
+  let nav = "#" + divs.html();
+  $(nav).toggle("1s");
+}
 
- $("#namemenu").toggle("3s");
+$("#outrosmenus").click(() => {
+  $("#namemenu").toggle("3s");
   $("#restomenus").toggle("3s");
+});
+
+$("#inventario-btn").click(() => {
+  abrir($("#inventario-btn"));
+});
+
+$("#loja-btn").click(() => {
+  abrir($("#loja-btn"));
+});
+
+$("#avatar-btn").click(() => {
+  abrir($("#avatar-btn"));
+});
+
+$("quests-btn").click(() => {
+  abrir($("#quests-btn"));
+});
+
+$("#creditos-btn").click(() => {
+  abrir($("#creditos-btn"));
 });
