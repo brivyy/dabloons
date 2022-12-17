05d255c75beff70400367dbe3c4cde3059d700bc
brivyy <vieirabruno28112005@gmail.com>
2022-12-17 13:12:51 -0300

    inventario 2

--- js/main.js
+++ js/main.js
@@ -2,6 +2,10 @@ function trocardisplay(divs) {
   let nav = "#" + divs.html();
   $(nav).toggle("1s");
 }
+function fechar(){
+
+  $(".navs").hide("1s");
+  }
 
 $("#outrosmenus").click(() => {
   $("#namemenu").toggle("3s");
@@ -9,26 +13,30 @@ $("#outrosmenus").click(() => {
 });
 
 $("#inventario-btn").click(() => {
+  fechar();
   trocardisplay($("#inventario-btn"));
 });
 
 $("#loja-btn").click(() => {
+  fechar();
   trocardisplay($("#loja-btn"));
 });
 
 $("#avatar-btn").click(() => {
+  fechar();
   trocardisplay($("#avatar-btn"));
 });
 
 $("#quests-btn").click(() => {
+  fechar();
   trocardisplay($("#quests-btn"));
 });
 
 $("#creditos-btn").click(() => {
+  fechar();
   trocardisplay($("#creditos-btn"));
 });
 
 $(".x").click(()=>{
-
-$(".navs").hide("1s");
+  fechar();
 })
